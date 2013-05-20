3f9547350d89a47b42d1eebe6500262925a8267b
rilian <dmitriyis@gmail.com>
2013-05-20 20:29:52 +0300

    fixing books spec

--- spec/unit/libclub/reader/books_spec.rb
+++ spec/unit/libclub/reader/books_spec.rb
@@ -1,10 +1,10 @@
 require 'spec_helper'
 
 describe Libclub::Reader, '#books' do
-  let(:instance)   { described_class.new('Barack') }
-  let(:subject)    { instance.books }
-  let(:book_one)   { mock('Book one') }
-  let(:book_two)   { mock('Book two') }
+  let(:instance) { described_class.new('Barack') }
+  let(:subject)  { instance.books }
+  let(:book_one) { mock('Book one') }
+  let(:book_two) { mock('Book two') }
   let(:book_three) { mock('Book three') }
 
   it 'should calculate presented books' do
@@ -13,7 +13,6 @@ describe Libclub::Reader, '#books' do
     instance.books_lent << book_two
     instance.books_borrowed << book_three
 
-    expect(subject).to include(book_one)
-    expect(subject).to include(book_three)
+    expect(subject).to eql([book_one, book_three])
   end
 end
